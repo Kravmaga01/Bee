306b675ae766f763ee2a3518e459575f8914a615
Kravmaga 01 <92754288+Kravmaga01@users.noreply.github.com>
2022-10-06 01:11:06 -0500

    add:

--- Angular/app/src/app/app.module.ts
+++ Angular/app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponentCliComponent } from './first-component-cli/first-component-cli.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponentCliComponent
   ],
   imports: [
     BrowserModule,
