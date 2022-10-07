d462ac3bee18acee214e7f5cad9931c483beb6c2
Kravmaga 01 <92754288+Kravmaga01@users.noreply.github.com>
2022-10-07 15:04:29 -0500

    final commit
    Se modifico el codigo para hacerlo tipamdolo

--- Angular/app/src/app/app.component.ts
+++ Angular/app/src/app/app.component.ts
@@ -3,18 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  counter = 0;
-  increment(){
-    this.counter++;
-  }
-  decrement(){
-    this.counter--;
-  }
+  public counter: number = 0;
 
-  reset(){
-    this.counter = 0;
+  public operators(valor: number) {
+    if (valor === 1) {
+      this.counter++;
+    }
+    if (valor === -1) {
+      this.counter--;
+    }
+    if (valor === 0) {
+      this.counter = 0;
+    }
   }
 }
