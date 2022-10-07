e5b46544134e476cda5b49c59e0bb7706a33ebc8
Kravmaga 01 <92754288+Kravmaga01@users.noreply.github.com>
2022-10-06 21:59:36 -0500

    Final commit miniProyectCounter

--- Angular/app/src/app/app.component.ts
+++ Angular/app/src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  counter = 0;
+  increment(){
+    this.counter++;
+  }
+  decrement(){
+    this.counter--;
+  }
+
+  reset(){
+    this.counter = 0;
+  }
 }
