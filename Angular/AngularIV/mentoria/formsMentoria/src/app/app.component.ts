d84f983027c8a3938fc5f5f6999226bbc3ddcb53
Kravmaga01 <edwardsalex8034@gmail.com>
2022-11-07 15:58:31 -0500

    [add][changue]  se implmento ONInit  cambiar el valor del objeto newCoutrie

--- Angular/AngularIV/mentoria/formsMentoria/src/app/app.component.ts
+++ Angular/AngularIV/mentoria/formsMentoria/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'formsMentoria';
+
+  ngOnInit(): void {
+    console.log(this.newContries.name);
+    this.newContries.name = 'Canada';
+  }
+  public newContries = {
+    name: 'Colombia',
+    capital: '',
+  };
+
+  private countries: object[] = [];
+
+  public saveCountry(): void {
+    this.newContries.capital = `City ${this.newContries.capital}`;
+    this.countries.push(this.newContries);
+    console.log(this.countries);
+  }
 }
